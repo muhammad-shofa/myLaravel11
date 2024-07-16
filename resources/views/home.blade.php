e325b1c07729a9ca88884bba1af0bfdb42553ae5
Muhammad Shofa <mshofa09@gmail.com>
2024-07-16 09:12:26 +0700

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="about">About</a>
+    <a href="gallery">Gallery</a>
+    <h1>Selamat datang di halaman pertama website laravel 11 saya!</h1>
+    <img src="img/Sag_A_.jpg" alt="Sagittarius A*" width="200">
+    <p>Sagittarius A* <br> <i>Iseng aja yang penting ada gambarnya</i></p>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
